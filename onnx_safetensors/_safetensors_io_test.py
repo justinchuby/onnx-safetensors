c7e9f1d988a5d522e3d77e7b805113a9bb7953ab
Justin Chu <justinchu@microsoft.com>
2023-08-01 23:47:01 +0000

    tests

---
+++ onnx_safetensors/_safetensors_io_test.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import unittest
+
+
+class SafeTensorsIoTest(unittest.TestCase):
+    def test_load_safetensors_file(self) -> None:
+        pass
+
+    def test_load_safetensors(self) -> None:
+        pass
+
+    def test_save_safetensors_file(self) -> None:
+        pass
