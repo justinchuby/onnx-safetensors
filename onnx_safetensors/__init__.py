bc5f818a316095c15db702c0bfb7b061f4401e87
Justin Chu <justinchuby@users.noreply.github.com>
2023-08-02 08:07:36 -0700

    feat: expose utilities for manipulating the model

--- onnx_safetensors/__init__.py
+++ onnx_safetensors/__init__.py
@@ -1,6 +1,8 @@
 """Use safetensors with ONNX."""
 
+from onnx_safetensors import utils
 from onnx_safetensors._safetensors_io import (
+    apply_tensors,
     load,
     load_file,
     save,
@@ -14,5 +16,7 @@ __all__ = [
     "save",
     "save_file",
     "strip_raw_data",
+    "apply_tensors",
+    "utils",
 ]
 __version__ = "0.1.0"
