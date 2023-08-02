e13956cc5fc5e99d46685e10392b9c7f481eae67
Justin Chu <justinchuby@users.noreply.github.com>
2023-08-01 18:45:14 -0700

    feat: update apis (#5)
    Test with a real model and updated apis to `load`, `save`, `load_file`,
    `save_file`.

--- onnx_safetensors/__init__.py
+++ onnx_safetensors/__init__.py
@@ -1,14 +1,18 @@
 """Use safetensors with ONNX."""
 
 from onnx_safetensors._safetensors_io import (
-    load_safetensors,
-    load_safetensors_file,
-    save_safetensors_file,
+    load,
+    load_file,
+    save,
+    save_file,
+    strip_raw_data,
 )
 
 __all__ = [
-    "load_safetensors_file",
-    "load_safetensors",
-    "save_safetensors_file",
+    "load",
+    "load_file",
+    "save",
+    "save_file",
+    "strip_raw_data",
 ]
 __version__ = "0.1.0"
